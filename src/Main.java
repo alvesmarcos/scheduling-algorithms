43aa6a3155ddaa1e160b3a825737c06e36d8b92a
Marcos Alves <alves.henrique.marcos@gmail.com>
2016-09-10 17:11:29 -0300

    Parsing jobs implemented

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-/* 
- * Date: September 10th, 2016
- * Author: Marcos Alves
-**/
+// Created By: Marcos alves
+// Created Date: Sept. 10th, 2016	  
+// Last Modified: Sept. 10th, 2016	      
 
 import java.util.*;
 import java.io.*;
@@ -9,11 +8,36 @@ import java.io.*;
 
 public class Main {
 	public static void main(String args[]){
-		try {
-			Scanner sc = new Scanner(new FileReader(args[0]));
+		String path = null;
+		List<Job> jobs = new ArrayList<Job>();
+		Scanner read = new Scanner(System.in);
+			
+		if(args.length == 0){
+			System.out.print("[path] ~> ");	
+			path = read.next();
+		}
+		else
+			path = args[0];
 
-		} catch(FileNotFoundException e){
-			throw new RuntimeException("File doesn't exist: "+ e);
+		try {
+			// optimize if/when necessary
+			String line = null;
+			String[] parts = null;
+			BufferedReader br = new BufferedReader(new FileReader(path)); 
+
+			while((line = br.readLine()) != null){
+				if(line.contains(" ")){
+					parts = line.split(" ");
+					jobs.add(new Job(Integer.parseInt(parts[0]), Integer.parseInt(parts[1])));
+				} else
+					throw new IllegalArgumentException("File "+path+" doens't contain [space]");
+			}	
+			
+		} catch(IOException e){
+			e.printStackTrace();
 		} 
+
+		for(Job e: jobs)
+			System.out.println(e.toString());
 	}
 }
