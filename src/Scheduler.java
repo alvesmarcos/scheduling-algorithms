f1fc6a41211c212ad82f0c0ba997b87d4c8cdd3c
Marcos Alves <alves.henrique.marcos@gmail.com>
2016-09-16 12:21:38 -0300

    Implemented FCFS and RR

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,6 +1,6 @@
 // Created By: Marcos alves
 // Created Date: Sept. 10th, 2016	  
-// Last Modified: Sept. 13th, 2016	      
+// Last Modified: Sept. 15th, 2016	      
 
 import java.util.*;
 import java.io.*;
@@ -8,30 +8,37 @@ import java.io.*;
 public class Scheduler {
 	//attributes
 	private float response, waiting, turnaround; //AVERAGE
-	private Routine r;
-	private List<Job> queue;
+	private Routine routine;
+	private Queue<Job> queue;
+	//print about Running jobs | true - print
+	protected static final boolean DEBUG = true;
 
 	//constructor
 	public Scheduler(Routine r){
 		response = waiting = turnaround = 0.0f;
-		this.r = r;
-		queue = new ArrayList<Job>();
+		this.routine = r;
+		queue = new LinkedList<Job>();
+		
+		//expect path ../jobs.txt
+		loaderJobs();
 	}
 	
 	//methods
-	//r = Routine.FCFS |
+	//routine = Routine.FCFS | 
 	private void firstComeFirstServed(){
-		int sum = 0, len = queue.size();
-		Job first = null;
-
-		for(int i = 0; i < len ; i++){
-			//TODO
+		int sum = 0, count = 0;
+		
+		while(!queue.isEmpty()){
+			if(DEBUG)
+				System.out.println("Running job (" + (count++) + ") " + sum + "-" + 
+							  	  (sum+queue.peek().getTime()));
+			sum += queue.peek().getTime();
+			queue.poll();
 		}
 	}
 
 	//loaderJobs expected command "java Main < jobs.txt" 
-	//public provisionally
-	public void loaderJobs(){
+	private void loaderJobs(){
 		//getting keyboard input 
 		Scanner reader = new Scanner(System.in);
 		//buffers
@@ -49,13 +56,17 @@ public class Scheduler {
 		}	
 	}
 
-	//print specific element by index (IF EXIST) 
+	//print specific element (IF EXIST)
 	public void peekJob(int index){
-		try {
-			System.out.println(queue.get(index).toString());
-		} catch (IndexOutOfBoundsException e){
-			e.printStackTrace();
-		}
+		int count = 1;
+
+		if(index > queue.size()){
+			for(Job j: queue){
+				if(count == index)
+					System.out.println(j.toString()); break;
+			}
+		} else
+			throw new IndexOutOfBoundsException("Outside range");
 	}
 
 	//print all elements of queue 
@@ -63,7 +74,48 @@ public class Scheduler {
 		queue.forEach(j->System.out.println(j.toString()));
 	}
 
+	//routine = Routine.RR | 
+	private void roundRobin(){
+		int sum = 0, count = 0;
+		final int quantum = 2; //IMPORTANT
+		Job first = null;
+
+		while(!queue.isEmpty()){
+			if(DEBUG)
+				System.out.println("Running job (" + (count++) + ") " + sum + "-" + 
+							  	  (sum+quantum));
+			sum += quantum;
+			//sub quantum of the job
+			queue.peek().subTime(quantum);
+			//return always first element 
+			first = queue.peek();
+			if(first.getTime() > 0)
+				queue.add(first);
+			//pop anyway
+			queue.poll();
+		}
+	}
+
+	//run scheduling-cpu
 	public void run(){
+		//test how routine enum
+		switch(routine){
+			case FCFS:
+				firstComeFirstServed();
+				break;
+			case SJF:
+				shortestJobFirst();
+				break;
+			case RR:
+				roundRobin();
+				break;
+			default: 
+				throw new IllegalArgumentException("Undefined reference routine!");
+		}
+	}
+
+	//routine = Routine.SJF | 
+	private void shortestJobFirst(){
 		//TODO
 	}
 }
